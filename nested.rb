593737b6e862f781fa59dca060885b69d27ac122
Ammar Ali <funnyguy118@gmail.com>
2019-01-30 00:08:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "", :languages => []}  
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-return programmer_hash
+programmer_hash
 end
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
